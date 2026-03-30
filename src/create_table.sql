f57148f44f0a96bf12dda804637bc530fe1632f3
zdx <2608204518@qq.com>
2026-03-30 19:16:25 +0800

    【add】新增

--- src/create_table.sql
+++ src/create_table.sql
@@ -67,7 +67,7 @@ create table stock_3days_down (
     industry_detail varchar(100)  comment '细分行业'
 );
 
--- 倍量回调 - 踩上分歧日 结果表
+-- 倍量回调 - 踩上分歧日 结果表（不指定返回目标天，dt 为分歧天）
 CREATE TABLE `stock_detail_calc` (
     `calc_dt` varchar(10) DEFAULT NULL COMMENT '计算日期，格式 yyyy-MM-dd',
     `dt` varchar(10) DEFAULT NULL COMMENT '日期，格式 yyyy-MM-dd',
@@ -85,4 +85,21 @@ CREATE TABLE `stock_detail_calc` (
     `turnover_rate` double DEFAULT NULL COMMENT '换手率',
     `industry` varchar(100) comment '所属行业',
     `industry_detail` varchar(100)  comment '细分行业'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 倍量回调 - 踩上分歧日 结果表（dt 为返回目标天）
+CREATE TABLE `stock_detail_calc_backtracking` (
+  `dt` varchar(10) DEFAULT NULL COMMENT '日期，格式 yyyy-MM-dd',
+  `code` varchar(6) DEFAULT NULL COMMENT '股票代码',
+  `stock_name` varchar(100) DEFAULT NULL COMMENT '股票名称',
+  `price_open` double DEFAULT NULL COMMENT '开盘价',
+  `price_close` double DEFAULT NULL COMMENT '收盘价',
+  `price_highest` double DEFAULT NULL COMMENT '最高价',
+  `price_lowest` double DEFAULT NULL COMMENT '最低价',
+  `trade` double DEFAULT NULL COMMENT '成交量(总手)',
+  `trade_amount` double DEFAULT NULL COMMENT '成交额',
+  `amplitude` double DEFAULT NULL COMMENT '振幅',
+  `rise` double DEFAULT NULL COMMENT '收盘涨幅',
+  `amount_increase_decrease` double DEFAULT NULL COMMENT '涨跌额',
+  `turnover_rate` double DEFAULT NULL COMMENT '换手率'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
